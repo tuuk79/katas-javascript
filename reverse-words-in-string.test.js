4fd3183425f9df4d3d6d369e284c42d56d67f183
steve.yoo@servicenow.com <steve.yoo@servicenow.com>
2020-01-21 16:15:19 -0600

    adding reverse words kata

---
+++ reverse-words-in-string.test.js
@@ -0,0 +1,29 @@
+// Given an input string, reverse the string word by word.
+
+function reverseWords(input) {
+  var splitted = input.split(/\s+/);
+  var reverse = [];
+
+  for (var i = splitted.length - 1; i >= 0; i--) {
+    reverse.push(splitted[i]);
+  }
+
+  return reverse.join(" ");
+}
+
+describe("reverse words in a string", () => {
+  it("should reverse the words", () => {
+    var input = "the best       word   period";
+    expect(reverseWords(input)).toBe("period word best the");
+  });
+
+  it("should reverse 'big bird' into 'bird big", () => {
+    var input = "big bird";
+    expect(reverseWords(input)).toBe("bird big");
+  });
+
+  it("should reverse '  hello world!  ' into 'world! hello", () => {
+    var input = "  hello world!  ";
+    expect(reverseWords(input)).toBe("world! hello");
+  });
+});
