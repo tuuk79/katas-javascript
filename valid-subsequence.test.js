1eeadfb2be08a1c8baca12616fb367effacb30a8
Steve Yoo <stevecyoo@stevecyoo.lan>
2020-11-13 21:49:31 -0600

    added valid subsequence kata

---
+++ valid-subsequence.test.js
@@ -0,0 +1,38 @@
+function isValidSubsequence(array, sequence) {
+  const filtered = sequence.filter(s => array.includes(s));
+
+  const check1 = array.length === filtered.length;
+  const check2 = filtered.every((val) => array.includes(val));
+
+  return check1 && check2;
+}
+
+describe("valid subsequence", () => {
+  it("[1] is a subsequence of [1]", () => {
+    const array = [1];
+    const sequence = [1];
+    const result = isValidSubsequence(array, sequence);
+    expect(result).toBe(true);
+  });
+
+  it("[1] is a subsequence of [1,2]", () => {
+    const array = [1];
+    const sequence = [1,2];
+    const result = isValidSubsequence(array, sequence);
+    expect(result).toBe(true);
+  });
+
+  it("[1] is not a subsequence of [2,3]", () => {
+    const array = [1];
+    const sequence = [2,3];
+    const result = isValidSubsequence(array, sequence);
+    expect(result).toBe(false);
+  });
+
+  it("[2,3] is a subsequence of [1,2,3]", () => {
+    const array = [2,3];
+    const sequence = [1,2,3];
+    const result = isValidSubsequence(array, sequence);
+    expect(result).toBe(true);
+  });
+});
