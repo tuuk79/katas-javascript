001fd30e68dd64a33a79558c18b353af17e6944e
Steve Yoo <stevecyoo@gmail.com>
2022-08-12 18:00:52 -0500

    added regex practice

---
+++ regex-practice.test.js
@@ -0,0 +1,9 @@
+describe("regex practice", () => {
+	it("should be true", () => {
+		const p =
+			"The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?";
+
+		console.log(p)
+		console.log(p.replace("dog", "monkey"));
+	});
+});
