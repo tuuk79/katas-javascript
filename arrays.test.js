2223f752300b6343d3a2601de95fdd5f418714bd
Steve Yoo <stevecyoo@gmail.com>
2024-06-16 06:12:49 -0500

    arrays practice

--- arrays.test.js
+++ arrays.test.js
@@ -2,6 +2,11 @@ function sum(arr) {
 	return arr.reduce((prev, curr) => prev + curr, 0)
 }
 
+function insert(input, value, index) {
+	input.splice(index,0,value)
+	return input
+}
+
 describe('arrays', () => {
 	it('should sum [1,2,3] to be 6', () => {
 		const arr = [1,2,3]
@@ -17,10 +22,17 @@ describe('arrays', () => {
 		expect(actual).toBe(expected)
 	})
 
-	it('', () => {
-		const names = ["steve", "yoo", "dohster"]
-		const actual = names.push("canned beans")
-		const expected = ["steve", "yoo", "dohster", "canned beans"]
+	it('should insert 4 at index 1 resulting in [1,4,2,3]', () => {
+		const input = [1,2,3]
+		const actual = insert(input, 4, 1)
+		const expected = [1,4,2,3]
+		expect(actual).toEqual(expected)
+	})
+
+	it('should insert 5 at index 2 resulting in [1,2,5,3]', () => {
+		const input = [1,2,3]
+		const actual = insert(input, 5, 2)
+		const expected = [1,2,5,3]
 		expect(actual).toEqual(expected)
 	})
 })
